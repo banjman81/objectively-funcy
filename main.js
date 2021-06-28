9e554cd8cee2355d6f9ae3c03dddba40d56437f5
[banjman81] <[benjamin.man97@gmail.com]>
2021-06-28 13:35:20 -0500

    done

--- main.js
+++ main.js
@@ -1,7 +1,46 @@
 // Your code here.
+function getFirstName(object){
+  return object.firstName
+}
 
+function getLastName(object){
+  return object.lastName
+}
 
+function getFullName(object){
+  return object.firstName + " " + object.lastName
+}
 
+function setFirstName(object, input){
+  object.firstName = input
+}
+
+function setAge(object, input){
+  object.age = input
+}
+
+function giveBirthday(object){
+  if(object.age === undefined){
+    object.age = 1
+  }
+  else{
+    object.age++
+  }
+}
+
+function marry(a, b){
+  a.married = true
+  b.married = true
+  a.spouseName =  b.firstName+ " " + b.lastName
+  b.spouseName =  a.firstName+ " " + a.lastName
+}
+
+function divorce(a, b){
+  a.married = false
+  b.married = false
+  delete a["spouseName"]
+  delete b["spouseName"]
+}
 
 
 // Our code here. Don't touch!
